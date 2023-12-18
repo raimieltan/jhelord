0165f519b3f94f10ad2d498e7fb8317637e4e3fc
raimieltan <raimieltan@gmail.com>
2023-12-18 15:23:10 +0800

    feat: backend edits

---
+++ client/components/profile/ProfileDriver.jsx
@@ -0,0 +1,140 @@
+// components/profile/Profile.js
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Alert } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation } from '@react-navigation/native';
+import { Avatar } from 'react-native-elements';
+
+const ProfileDriver = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [role, setRole] = useState('')
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    fetchUserProfile();
+  }, []);
+
+  const fetchUserProfile = async () => {
+    try {
+      const token = await AsyncStorage.getItem('accessToken');
+
+      if (!token) {
+        navigation.navigate('LOGIN');
+        return;
+      }
+
+      const response = await fetch('https://jhelord-backend.onrender.com/api/users/profile', {
+        method: 'GET',
+        headers: {
+          Authorization: token,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch user profile');
+      }
+
+      const userProfile = await response.json();
+      setUsername(userProfile.username);
+      setEmail(userProfile.email);
+      setRole(userProfile.role)
+    } catch (error) {
+      console.error('Error fetching user profile:', error.message);
+      Alert.alert('Error', 'Failed to fetch user profile');
+    }
+  };
+
+  const handleLogout = async () => {
+    try {
+      await AsyncStorage.removeItem('accessToken');
+      navigation.navigate('Login');
+    } catch (error) {
+      console.error('Error during logout:', error.message);
+      Alert.alert('Error', 'Failed to logout');
+    }
+  };
+
+  const handleNavigateToMap = () => {
+    navigation.navigate('Map');
+  };
+  const handleNavigateToCreateDriver = () => {
+    navigation.navigate('CreateProfile');
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Avatar
+          size="large"
+          rounded
+          source={{ uri: 'https://placekitten.com/200/200' }} // Replace with the user's profile picture URL
+          containerStyle={styles.avatar}
+        />
+        <View style={styles.userInfo}>
+          <Text style={styles.username}>{username}</Text>
+          <Text style={styles.email}>{email}</Text>
+          <Text style={styles.email}>{role}</Text>
+        </View>
+      </View>
+      <TouchableOpacity style={styles.mapButton} onPress={handleNavigateToMap}>
+        <Text style={styles.mapButtonText}>Go to Map</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.mapButton} onPress={handleNavigateToCreateDriver}>
+        <Text style={styles.mapButtonText}>Create Profile</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
+        <Text style={styles.logoutButtonText}>Logout</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  avatar: {
+    marginRight: 15,
+  },
+  userInfo: {
+    flex: 1,
+  },
+  username: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  email: {
+    fontSize: 16,
+    color: 'gray',
+  },
+  mapButton: {
+    backgroundColor: '#3498db',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  mapButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  logoutButton: {
+    backgroundColor: '#d9534f',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  logoutButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});
+
+export default ProfileDriver;
--- client/navigation/MainStackNavigator.jsx
+++ client/navigation/MainStackNavigator.jsx
@@ -1,17 +1,58 @@
 // navigation/MainStackNavigator.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { StyleSheet, View, Image, Text } from 'react-native';
 import Profile from '../components/profile/Profile';
 import MapHome from '../components/map/InitialMapScreen';
 import Login from '../components/login/Login';
 import Map from '../components/map/Map';
 import Signup from '../components/login/Signup';
+import CreateDriverProfile from '../components/profile/CreateDriverProfile';
+import ProfileDriver from '../components/profile/ProfileDriver';
 const Stack = createStackNavigator();
 
 const MainStackNavigator = () => {
+
+    const [userRole, setUserRole] = useState(null);
+
+
+    const fetchUserProfile = async () => {
+        try {
+            const token = await AsyncStorage.getItem('accessToken');
+
+            if (!token) {
+                navigation.navigate('LOGIN');
+                return;
+            }
+
+            const response = await fetch('https://jhelord-backend.onrender.com/api/users/profile', {
+                method: 'GET',
+                headers: {
+                    Authorization: token,
+                },
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to fetch user profile');
+            }
+
+            const userProfile = await response.json();
+            setUserRole(userProfile.role);
+            console.log(userProfile.role)
+
+        } catch (error) {
+            console.error('Error fetching user profile:', error.message);
+            Alert.alert('Error', 'Failed to fetch user profile');
+        }
+    };
+
+
+    useEffect(() => {
+        fetchUserProfile()
+    }, [])
+
     return (
         <Stack.Navigator>
             <Stack.Screen
@@ -31,7 +72,7 @@ const MainStackNavigator = () => {
                 }}
 
             />
-                    <Stack.Screen
+            <Stack.Screen
                 name="Signup"
                 component={Signup}
                 options={{
@@ -55,13 +96,13 @@ const MainStackNavigator = () => {
                     title: '',
                     headerStyle: {
                         backgroundColor: '#039043',
-                  
+
                     },
                     headerTintColor: '#fff',
                     headerTitleStyle: {
                         fontWeight: 'bold',
                     },
-                    
+
                 }}
             />
             <Stack.Screen
@@ -71,21 +112,21 @@ const MainStackNavigator = () => {
                     title: 'Taxis',
                     headerStyle: {
                         backgroundColor: '#039043',
-                  
+
                     },
                     headerTintColor: '#fff',
                     headerTitleStyle: {
                         fontWeight: 'bold',
                     },
                     headerShown: false
-                    
+
                 }}
             />
             <Stack.Screen
                 name="Profile"
-                component={Profile}
+                component={userRole === 'user' ? Profile : ProfileDriver}
                 options={{
-                    title: 'Profile',
+                    title: userRole === 'user' ? 'Profile' : userRole === 'driver' ? 'Driver Profile' : '',
                     headerStyle: {
                         backgroundColor: '#039043',
                     },
@@ -94,9 +135,26 @@ const MainStackNavigator = () => {
                         fontWeight: 'bold',
                     },
                     headerTitleAlign: 'center',
-                    headerShown: false
+                    headerShown: userRole === 'driver'
                 }}
             />
+            <Stack.Screen
+                name="CreateProfile"
+                component={CreateDriverProfile}
+                options={{
+                    title: 'Create Profle',
+                    headerStyle: {
+                        backgroundColor: '#039043',
+                    },
+                    headerTintColor: '#fff',
+                    headerTitleStyle: {
+                        fontWeight: 'bold',
+                    },
+                    headerTitleAlign: 'center',
+                    headerShown: userRole === 'driver'
+                }}
+            />
+
         </Stack.Navigator>
     );
 };
