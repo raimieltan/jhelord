a14f80d36e403c0b414adfc569dced6fb7cc5b34
Michael Handumon <kmcaelian@gmail.com>
2023-12-19 17:12:53 +0800

    feat: implemented unit table and operation table on admin page

--- admin/app/(main)/admin/page.tsx
+++ admin/app/(main)/admin/page.tsx
@@ -1,25 +1,91 @@
-import { UnitTable } from "@/app/components/unit/UnitTable";
+"use client"
+
+import UnitTable from "@/app/components/unit/UnitTable";
+import Map from "@/app/components/map/map";
+import { useEffect, useState } from "react";
+import { Unit } from "@/app/types/unit";
+import OperationTable from "@/app/components/operation/OperationTable";
+import { Operation } from "@/app/types/operation";
+
+enum TableType {
+  units,
+  operations
+}
 
 export default function Admin() {
+  const [units, setUnits] = useState<Unit[]>([]);
+  const [operations, setOperations] = useState<Operation[]>([]);
+  const [tableToggle, setTableToggle] = useState<TableType>(TableType.units);
+
+  const fetchUnits = async () => {
+    try {
+      const response = await fetch(`http://localhost:8000/api/units`)
+      const data = await response.json()
+      setUnits(data);
+
+    } catch (error: any) {
+      console.log(error)
+    }
+  }
+
+  const fetchOperations = async () => {
+    try {
+      const response = await fetch(`http://localhost:8000/api/operations`)
+      const data = await response.json()
+      setOperations(data);
+
+    } catch (error: any) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchUnits();
+    fetchOperations();
+  }, [])
+
   return (
-    <div className="flex w-full h-full bg-white">
-      <div className="flex flex-col w-full">
-        <div className="flex flex-row border border-black m-4 text-2xl font-semibold">
+    <div className="w-full flex flex-col h-full">
+      <div className="flex h-96 bg-white w-full">
+        <Map units={units} />
+      </div>
+
+      <div className="text-black flex flex-row w-full font-semibold">
+        <div className="text-center border-2 rounded-2xl p-4 m-4 bg-green-400 shadow-sm shadow-black flex items-center">
+          <p>Operational Units: {units.length}</p>
+        </div>
+        <div className="text-center border-2 rounded-2xl p-4 m-4 bg-blue-400 shadow-sm shadow-black flex items-center">
+          <p>Occupied: 2</p>
+        </div>
+      </div>
+
+      <div>
+        <div className="flex flex-row text-2xl font-semibold mx-4 my-4">
           <button
-            className="border border-red-500 w-full text-black bg-gray-300">
-            {/* TODO: onclick toggle */}
+            onClick={() => setTableToggle(TableType.units)}
+            disabled={tableToggle === TableType.units}
+            className={`shadow-black shadow-sm w-full text-black bg-gray-300 transition-all rounded-md mx-2 ${tableToggle === TableType.units ? "bg-green-600" : "hover:bg-gray-500 bg-gray-400"}`}>
             Units
           </button>
-          <button className="border border-blue-500 w-full text-black bg-gray-300">
-            {/* TODO: onclick toggle */}
+          <button
+            onClick={() => setTableToggle(TableType.operations)}
+            disabled={tableToggle === TableType.operations}
+            className={`shadow-black shadow-sm w-full text-black bg-gray-300 transition-all rounded-md mx-2 ${tableToggle === TableType.operations ? "bg-green-600" : "hover:bg-gray-500 bg-gray-400"}`}>
             Logs
           </button>
         </div>
-
-        <div className="w-full h-100% p-4">
-          <UnitTable />
-        </div>
       </div>
-    </div>
+
+      {
+        tableToggle === TableType.units ?
+          <div className="flex flex-col mx-4">
+            <UnitTable units={units} />
+          </div>
+          :
+          <div className="flex flex-col mx-4">
+            <OperationTable operations={operations} />
+          </div>
+      }
+    </div >
   )
 }
--- admin/app/(main)/admin/unit/[unitId]/page.tsx
+++ admin/app/(main)/admin/unit/[unitId]/page.tsx
@@ -3,6 +3,9 @@
 import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation"
 import { Unit } from "@/app/types/unit";
+import Image from "next/image";
+import Map from "@/app/components/map/map";
+import UnitMap from "@/app/components/map/unitMap";
 
 export const UnitPage = () => {
   const unitParamsId = usePathname().split("/")[3]
@@ -11,11 +14,10 @@ export const UnitPage = () => {
   const fetchUnit = async (unitId: string) => {
     if (unitId) {
       try {
-        const response = await fetch(`http://localhost:8000/api/units/${unitId}`)
+        const response = await fetch(`http://localhost:8000/api/units/get-unit/${unitId}`)
         const data = await response.json()
+        console.log("DATA: ", data);
         setUnit(data);
-
-        console.log(data)
 
       } catch (error: any) {
         console.log(error)
@@ -23,44 +25,58 @@ export const UnitPage = () => {
     }
   }
 
-  console.log(unit?.driver);
-
   useEffect(() => {
     fetchUnit(unitParamsId);
-  }, [])
+  }, [unitParamsId])
 
   return (
     unit &&
-    <div className="flex w-full h-full bg-white">
-      <div className="flex flex-col w-full">
-        <div className="flex flex-col border border-black h-full m-4 text-2xl font-semibold">
-          <div className="flex flex-row border border-red-500 m-4 text-black justify-between">
-            <div className="flex flex-row border border-blue-500">
-              <div className="p-28">
-                Driver Image
-              </div>
-              {
-                unit ?
-                  <div className="flex flex-col">
-                    <p className="my-2">Driver: {unit.driver?.firstName} {unit.driver?.lastName}</p>
-                    <p className="my-2">Number: Unit Number</p>
-                    <p className="my-2">Plate Number: Unit Plate Number</p>
-                    <p className="my-2">Make and Model: Unit make and model</p>
-                    <p className="my-2"> View License </p>
-                  </div>
-                  : ''
-              }
+    <div className="flex flex-col bg-white">
+      <div className="flex flex-row w-full h-screen">
+        <div className="flex flex-col p-4 m-4 rounded-xl bg-gray-300 shadow-black shadow-md border">
+          <div className="text-black font-semibold text-center text-2xl my-4">
+            <p>Unit Info</p>
+          </div>
+
+          <div className="w-80">
+            <img src="/taxi_placeholder.webp" alt="driver" />
+          </div>
+
+          <div className="text-green-800 font-semibold text-center text-xl my-4">
+            <p>View License</p>
+          </div>
+
+          <div className="mt-12">
+            <div className="bg-gray-400 rounded-xl text-black font-semibold text-center p-2 mx-2">
+              <p>{unit.driver.firstName} {unit.driver?.lastName}</p>
             </div>
-            <div className="border border-yellow-300 mx-4">
-              <p className="my-2">Status: Unit Status</p>
+
+            <div className="bg-gray-400 rounded-xl text-black font-semibold text-center p-2 m-2">
+              <p>{unit.make} {unit.model}</p>
+            </div>
+
+            <div className="flex flex-row text-black font-semibold justify-between">
+              <div className="bg-gray-400 rounded-xl flex items-center p-2 mx-2">
+                <p>Unit {unit.number}</p>
+              </div>
+              <div className="bg-gray-400 rounded-xl flex items-center p-2 mx-2">
+                <p>Plate Number: {unit.plateNumber}</p>
+              </div>
+            </div>
+
+            <div className={`bg-gray-400 rounded-xl ${unit.status === 'Operational' ? 'text-green-900' : 'text-black'} font-semibold text-center p-2 m-2`}>
+              <p>{unit.status}</p>
             </div>
           </div>
 
-          <div className="border border-yellow-500 m-4 h-full text-black">
-            Map
-          </div>
+        </div>
+        <div className="border w-full p-4">
+          <UnitMap unit={unit} />
         </div>
       </div>
+      {/* <div className="border border-red-500">
+
+      </div> */}
     </div>
   );
 };
