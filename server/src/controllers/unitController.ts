c20da449d94defe55d84375aef7ec0bdea40c45e
Michael Handumon <kmcaelian@gmail.com>
2024-01-07 13:44:38 +0800

    chore: console logs cleanup

--- server/src/controllers/unitController.ts
+++ server/src/controllers/unitController.ts
@@ -10,7 +10,5 @@ export const getUnitById = async (req: Request, res: Response) => {
   const unitId = parseInt(req.params.id);
   const units = await retrieveUnits(unitId);
 
-  console.log("UNITS: ", units);
-
   return res.json(units);
 }
