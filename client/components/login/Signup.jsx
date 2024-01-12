8159f5d136c1e70fbc8734384f1c138a61543d09
raimieltan <raimieltan@gmail.com>
2024-01-12 14:03:11 +0800

    feat: user routes

--- client/components/login/Signup.jsx
+++ client/components/login/Signup.jsx
@@ -1,26 +1,26 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert, Image } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert, Image, ScrollView } from 'react-native';
 import Icon from 'react-native-vector-icons/AntDesign';
 import Logo from '../../assets/images/logo/logo.png';
 import { useNavigation } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Signup = () => {
-    const [username, setUsername] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [phoneNumber, setPhoneNumber] = useState('');
-    const [firstName, setFirstName] = useState('')
-    const [lastName, setLastName] = useState('')
-    const [profileImage, setProfileImage] = useState('')
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [profileImage, setProfileImage] = useState('')
 
 
   const navigation = useNavigation();
 
   const handleSignup = async () => {
     try {
-    
-    await AsyncStorage.removeItem('accessToken');
+
+      await AsyncStorage.removeItem('accessToken');
       const response = await fetch('https://jhelord-backend.onrender.com/api/users/signup', {
         method: 'POST',
         headers: {
@@ -59,64 +59,70 @@ const Signup = () => {
         </Text>
       </View>
 
-      <View style={styles.inputContainer}>
-        <Icon name="user" size={20} style={styles.icon} />
-        <TextInput
-          style={styles.input}
-          placeholder="Firstname"
-          value={firstName}
-          onChangeText={setFirstName}
-        />
-      </View>
 
-      <View style={styles.inputContainer}>
-        <Icon name="user" size={20} style={styles.icon} />
-        <TextInput
-          style={styles.input}
-          placeholder="Lastname"
-          value={lastName}
-          onChangeText={setLastName}
-        />
-      </View>
-      <View style={styles.inputContainer}>
-        <Icon name="user" size={20} style={styles.icon} />
-        <TextInput
-          style={styles.input}
-          placeholder="Username"
-          value={username}
-          onChangeText={setUsername}
-        />
-      </View>
+      <ScrollView style={{
+        width: '100%'
+      }}>
+        <View style={styles.inputContainer}>
+          <Icon name="user" size={20} style={styles.icon} />
+          <TextInput
+            style={styles.input}
+            placeholder="Firstname"
+            value={firstName}
+            onChangeText={setFirstName}
+          />
+        </View>
 
-      <View style={styles.inputContainer}>
-        <Icon name="mail" size={20} style={styles.icon} />
-        <TextInput
-          style={styles.input}
-          placeholder="Email"
-          value={email}
-          onChangeText={setEmail}
-        />
-      </View>
+        <View style={styles.inputContainer}>
+          <Icon name="user" size={20} style={styles.icon} />
+          <TextInput
+            style={styles.input}
+            placeholder="Lastname"
+            value={lastName}
+            onChangeText={setLastName}
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <Icon name="user" size={20} style={styles.icon} />
+          <TextInput
+            style={styles.input}
+            placeholder="Username"
+            value={username}
+            onChangeText={setUsername}
+          />
+        </View>
 
-      <View style={styles.inputContainer}>
-        <Icon name="mobile1" size={20} style={styles.icon} />
-        <TextInput
-          style={styles.input}
-          placeholder="Phone number"
-          value={phoneNumber}
-          onChangeText={setPhoneNumber}
-        />
-      </View>
-      <View style={styles.inputContainer}>
-        <Icon name="lock" size={20} style={styles.icon} />
-        <TextInput
-          style={styles.input}
-          placeholder="Password"
-          value={password}
-          onChangeText={setPassword}
-          secureTextEntry
-        />
-      </View>
+        <View style={styles.inputContainer}>
+          <Icon name="mail" size={20} style={styles.icon} />
+          <TextInput
+            style={styles.input}
+            placeholder="Email"
+            value={email}
+            onChangeText={setEmail}
+          />
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Icon name="mobile1" size={20} style={styles.icon} />
+          <TextInput
+            style={styles.input}
+            placeholder="Phone number"
+            value={phoneNumber}
+            onChangeText={setPhoneNumber}
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <Icon name="lock" size={20} style={styles.icon} />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry
+          />
+        </View>
+      </ScrollView>
+
       <TouchableOpacity style={styles.button} onPress={handleSignup}>
         <Text style={styles.buttonText}>Sign Up</Text>
       </TouchableOpacity>
@@ -129,72 +135,72 @@ const Signup = () => {
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      width: '100%',
-      padding: 20,
-      backgroundColor: 'white'
-  
-    },
-    image: {
-      width: '10%',
-      height: '10%',
-    
-    },
-    title: {
-      fontSize: 24,
-      marginBottom: 20,
-      fontWeight: 'bold',
-    },
-    greenBackground: {
-      backgroundColor: '#039043',
-      width: '100%',
-      height: 100, // Adjust the height as needed
-    },
-  
-    termsContainer: {
-      justifyContent: 'center',
-      alignItems: 'center', // Center text horizontally
-      width: '70%',
-  
-    },
-    termsText: {
-      textAlign: 'center', // Text alignment to center
-  
-    },
-    inputContainer: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      width: '100%',
-      borderColor: 'gray',
-      borderBottomWidth: 1,
-      marginVertical: 10,
-  
-    },
-    icon: {
-      marginRight: 10,
-    },
-    input: {
-      flex: 1,
-      height: 40,
-      paddingLeft: 10,
-      marginHorizontal: 10,
-    },
-    button: {
-      backgroundColor: '#039043',
-      padding: 10,
-      borderRadius: 5,
-      width: '100%',
-      marginTop: 20,
-    },
-    buttonText: {
-      color: 'white',
-      textAlign: 'center',
-      fontWeight: 'bold',
-    }
-  });
-  
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    padding: 20,
+    backgroundColor: 'white'
+
+  },
+  image: {
+    width: '10%',
+    height: '10%',
+
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+    fontWeight: 'bold',
+  },
+  greenBackground: {
+    backgroundColor: '#039043',
+    width: '100%',
+    height: 100, // Adjust the height as needed
+  },
+
+  termsContainer: {
+    justifyContent: 'center',
+    alignItems: 'center', // Center text horizontally
+    width: '70%',
+
+  },
+  termsText: {
+    textAlign: 'center', // Text alignment to center
+
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
+    borderColor: 'gray',
+    borderBottomWidth: 1,
+    marginVertical: 10,
+
+  },
+  icon: {
+    marginRight: 10,
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    paddingLeft: 10,
+    marginHorizontal: 10,
+  },
+  button: {
+    backgroundColor: '#039043',
+    padding: 10,
+    borderRadius: 5,
+    width: '100%',
+    marginTop: 20,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+    fontWeight: 'bold',
+  }
+});
+
 
 export default Signup;
--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -8,7 +8,6 @@ router.post('/signup', userController.signup);
 router.post('/login', userController.login);
 router.post('/users', userController.createUser);
 router.get('/:userId/profile', authMiddleware.authenticateToken, userController.getUserProfile);
-router.put('/users/:userId/profile', authMiddleware.authenticateToken, userController.updateUserProfile);
 router.delete('/users/:userId', authMiddleware.authenticateToken, userController.deleteUser);
 router.get('/profile', authMiddleware.authenticateToken, userController.getUserFromToken);
 export default router;
