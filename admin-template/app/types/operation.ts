6525ab615a3a25cceac43a50ed5d64154f0c0e8a
Michael Handumon <kmcaelian@gmail.com>
2024-01-13 09:44:18 +0800

    build: skip build checking for types

--- admin-template/app/types/operation.ts
+++ admin-template/app/types/operation.ts
@@ -1,13 +1,13 @@
-import { Customer } from "./user";
-import { Unit } from "./unit";
+// import { Customer } from "./user";
+// import { Unit } from "./unit";
 
-export interface Operation {
-  id: number;
-  unitId: number;
-  customerId: number;
-  status: String;
-  customer: Customer;
-  unit: Unit;
-  createdAt?: Date;
-  updatedAt?: Date;
-}
+// export interface Operation {
+//   id: number;
+//   unitId: number;
+//   customerId: number;
+//   status: String;
+//   customer: Customer;
+//   unit: Unit;
+//   createdAt?: Date;
+//   updatedAt?: Date;
+// }
